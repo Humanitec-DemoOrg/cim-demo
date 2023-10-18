4ea5880336a51b465cfc90dc701757393d799cb8
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-10-18 16:53:56 -0600

    no message

---
+++ resource-definition/role.tf
@@ -0,0 +1,45 @@
+resource "humanitec_resource_definition" "role" {
+  driver_type = "humanitec/terraform"
+  id          = "${local.app}-role"
+  name        = "${local.app}-role"
+  type        = "aws-role"
+
+  driver_inputs = {
+    secrets = {
+      variables = jsonencode({
+        access_key      = var.access_key
+        secret_key      = var.secret_key
+        assume_role_arn = var.assume_role_arn
+        region          = var.region
+      })
+    },
+    values = {
+      "source" = jsonencode(
+        {
+          path = "terraform/role/"
+          rev  = "refs/heads/main"
+          url  = "https://github.com/nickhumanitec/examples.git"
+        }
+      )
+      "variables" = jsonencode(
+        {
+          policy_arns = "$${resources.workload>aws-policy.outputs.arn}"
+          name        = "$${context.app.id}-$${context.env.id}-$${context.res.id}"
+          app         = "$${context.app.id}"
+          env         = "$${context.env.id}"
+          res         = "$${context.res.id}"
+        }
+      )
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      criteria
+    ]
+  }
+}
+
+resource "humanitec_resource_definition_criteria" "role" {
+  resource_definition_id = humanitec_resource_definition.role.id
+  app_id                 = humanitec_application.app.id
+}
