0d1219580abb5dd5193fc5ad856ed791767fa4f0
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-10-18 21:39:13 -0600

    no message

---
+++ resource-definition/ingress.tf
@@ -0,0 +1,74 @@
+variable "ingress_resource_name" {}
+variable "ingress_group_name" {}
+variable "ingress_cert_arn" {}
+
+resource "humanitec_resource_definition" "ingress" {
+  id          = var.ingress_resource_name
+  name        = var.ingress_resource_name
+  type        = "ingress"
+  driver_type = "humanitec/ingress"
+
+  driver_inputs = {
+    values = {
+      "annotations" : jsonencode({
+        "alb.ingress.kubernetes.io/certificate-arn" : "${var.ingress_cert_arn}",
+        "alb.ingress.kubernetes.io/group.name" : "${var.ingress_group_name}",
+        "alb.ingress.kubernetes.io/listen-ports" : "[{\"HTTP\":80},{\"HTTPS\":443}]",
+        "alb.ingress.kubernetes.io/scheme" : "internet-facing",
+        "alb.ingress.kubernetes.io/ssl-redirect" : "443",
+        "alb.ingress.kubernetes.io/target-type" : "ip"
+      }),
+      #   "api_version" : "v1",
+      "class" : "alb",
+      "no_tls" : true
+    }
+    secrets = {
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      criteria
+    ]
+  }
+}
+
+
+resource "humanitec_resource_definition_criteria" "ingress" {
+  resource_definition_id = humanitec_resource_definition.ingress.id
+  app_id                 = humanitec_application.app.id
+}
+
+//DNS
+
+variable "dns_shared_resource_name" {}
+variable "dns_shared_domain" {}
+
+resource "humanitec_resource_definition" "dns" {
+  id          = var.dns_shared_resource_name
+  name        = var.dns_shared_resource_name
+  type        = "dns"
+  driver_type = "humanitec/dns-wildcard"
+
+  driver_inputs = {
+    values = {
+      "domain" : var.dns_shared_domain,
+      "template" : "$${context.app.id}-$${context.env.id}"
+    }
+    secrets = {
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      criteria
+    ]
+  }
+
+}
+
+
+resource "humanitec_resource_definition_criteria" "dns" {
+  resource_definition_id = humanitec_resource_definition.dns.id
+  app_id                 = humanitec_application.app.id
+  res_id                 = "shared.${var.dns_shared_resource_name}"
+}
---
+++ score/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+
+export HUMANITEC_TOKEN="xxx"
+export HUMANITEC_ORG="xxx"
+export HUMANITEC_APP=myapp
+
+
+if [[ $1 == "httpd" ]]
+then
+    score-humanitec delta --token $HUMANITEC_TOKEN --org $HUMANITEC_ORG --app $HUMANITEC_APP --env development -f score-$1.yaml --extensions extension-$1.yaml --deploy
+else
+    score-humanitec delta --token $HUMANITEC_TOKEN --org $HUMANITEC_ORG --app $HUMANITEC_APP --env development -f score-$1.yaml --deploy
+fi
