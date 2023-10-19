213ee69887fcd34c22396c63d3285919482976a6
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-10-18 18:01:25 -0600

    updates

--- terraform/sqs/prod/main.tf
+++ terraform/sqs/prod/main.tf
@@ -17,7 +17,7 @@ output "name" {
 }
 
 locals {
-  sanitized_name   = replace(replace(lower(replace(replace(var.name, ".modules.", "-m-"), ".externals.", "-e-")), "/[^a-z\\-0-9]/", "-"), "/-*$/", "") #https://github.com/edgelaboratories/terraform-short-name/blob/main/main.tf
+  sanitized_name   = replace(replace(replace(replace(lower(var.name), ".modules.", "-m-"), ".externals.", "-e-"), "/[^a-z\\-0-9]/", "-"), "/-*$/", "") #https://github.com/edgelaboratories/terraform-short-name/blob/main/main.tf
   name_is_too_long = length(local.sanitized_name) > 40
   truncated_name   = replace(substr(local.sanitized_name, 0, 40 - 1 - 0), "/-*$/", "")
   name             = local.name_is_too_long ? local.truncated_name : local.sanitized_name
